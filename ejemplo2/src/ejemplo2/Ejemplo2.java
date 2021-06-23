944626eff9d723b5367ad1c01499d5c13564c3ee
Mario_cane.do@hotmaill.com <Mario_cane.do@hotmail.com>
2021-06-23 09:10:55 -0400

    cambios

--- ejemplo2/src/ejemplo2/Ejemplo2.java
+++ ejemplo2/src/ejemplo2/Ejemplo2.java
@@ -3,21 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejemplogit1;
+package ejemplo2;
 
 /**
  *
  * @author PC1
  */
-public class Ejemplogit1 {
+public class Ejemplo2 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        //hola.......
-
+        // TODO code application logic here
     }
     
 }
